d800a8cf3558a091734fe4d889bf8d659c3d361d
Miguel Ángel <19872243+Miguel-ASM@users.noreply.github.com>
2020-01-14 16:54:12 +0100

    Added propagation.py module
    Common Changes: added docstrings
    
    Also refactored the propagation.py module

--- pysom/grid.py
+++ pysom/grid.py
@@ -1,22 +1,25 @@
-################################################################################
-#                                       pySOM                                  #
-#------------------------------------------------------------------------------#
-#   Mikel Palmero  &  Miguel Angel Simon Martinez                              #
-################################################################################
+"""
+Definition of a position/momentum grid
+======================================
 
+functions to define the position/momentum grid
+"""
 
 import numpy as np
+from scipy import constants
 
-
-
-# function to define the grid
 def grid_def(nx, Lx0, Lx, hb):
+    """
+    Function that defines a fixed one-dimensional grid for momentum and position
+    """
+
+    pi = np.pi
+    hb = constants.hbar
 
     dx = Lx/(nx-1)                              #size of the step in x
-    # x = Lx0+dx*np.arange(1,nx)                #definition of the coordinate
     x = np.linspace(Lx0, Lx0+Lx, nx)
-    dpx = 2*np.pi*hb/(nx*dx)                    #size of the step in the position
 
+    dpx = 2*np.pi*hb/(nx*dx)                    #size of the step in the position
     px = dpx*np.arange(nx/2+1)
     px = np.concatenate((px[:-1],-px[-1:0:-1])) #definition of the momentum
 
---
+++ pysom/propagation.py
@@ -0,0 +1,36 @@
+"""
+Propagation of the wave functions
+=================================
+
+functions to propagate wave functions
+"""
+
+import numpy as np
+from scipy import constants
+
+def suzuki_trotter(psi, psip, expkin, V, n, nx, x, dx, px, dpx):
+    """
+    Function that evolves a one-dimensional wave-function using 2nd order Suzuki-Trotter
+    """
+
+    pi = np.pi
+    hb = constants.hbar
+
+    for i in range(n):
+        # First step of the SOM
+        psip *= expkin
+        psi = nx*np.fft.ifft(psip*dpx)/(2*pi*hb) #inverse Fourier transform
+
+        # Second step of the SOM
+        psi *= np.exp(-1j*V*dt/hb)
+        psip = np.fft.fft(psi*dx)/(2*pi*hb) #Fourier transform
+
+        # Last Step of the SOM
+        psip *= expkin
+        psi = nx*np.fft.ifft(psip*dpx)/(2*pi*hb) #inverse transform
+
+        #Normalize everything
+        psi /= np.sqrt(np.trapz(np.absolute(psi)**2, x) )
+        psip /= np.sqrt(np.trapz(np.absolute(psip)**2, px) )
+
+    return psi, psip
